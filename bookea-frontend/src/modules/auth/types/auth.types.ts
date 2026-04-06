235ac66a9d0892cbac661812123f7932c3076195
Mariannem <marianne.anjum@gmail.com>
2026-04-06 16:05:08 +0200

    feat(auth): add RegisterPayload type

--- bookea-frontend/src/modules/auth/types/auth.types.ts
+++ bookea-frontend/src/modules/auth/types/auth.types.ts
@@ -2,3 +2,16 @@ export interface LoginPayload {
     username: string;
     password: string;
 }
+
+export interface RegisterPayload {
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  address: string;
+  postalCode: string;
+  username: string;
+  email: string;
+  phone?: string;
+  password: string;
+  confirmPassword: string;
+}
